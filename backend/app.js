c340416f3f69182f42174f95c3a77f3ee93b8e8f
danygot18 <danygot74@gmail.com>
2023-11-01 16:17:56 +0800

    server setup

---
+++ backend/app.js
@@ -0,0 +1,17 @@
+const express = require('express');
+const app = express();
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+
+
+app.use(cors())
+app.use(express.json({limit:'50mb'}));
+app.use(express.urlencoded({limit: "50mb", extended: true }));
+app.use(cookieParser());
+
+app.get('/order',(req, res) => {
+    res.send('GUMANA NAA')
+})
+
+
+module.exports = app
