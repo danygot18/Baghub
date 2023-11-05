ca83cf6d2e430a26bcc119a1ba6e8be6db5afc5a
danygot18 <danygot74@gmail.com>
2023-11-05 15:17:31 +0800

    Logins/Logout

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -2,6 +2,7 @@ const User = require('../models/user');
 const sendToken = require('../utils/jwtToken');
 const cloudinary = require('cloudinary')
 const crypto = require('crypto')
+const sendEmail = require('../utils/sendEmail');
 
 exports.registerUser = async (req, res, next) => {
     const result = await cloudinary.v2.uploader.upload(req.file.path, {
@@ -33,4 +34,115 @@ exports.registerUser = async (req, res, next) => {
     }
     sendToken(user, 200, res)
 
+}
+
+exports.LoginUser = async (req, res, next) => {
+    const { email, password } = req.body;
+
+    // Checks if email and password is entered by user
+    if (!email || !password) {
+        return res.status(400).json({ error: 'Please enter email & password' })
+    }
+    // if (!email || !password) {
+    //     return next(new ErrorHandler('Please enter email & password', 400))
+    // }
+
+    // Finding user in database
+    const user = await User.findOne({ email }).select('+password')
+
+    if (!user) {
+        return res.status(401).json({ message: 'Invalid Email or Password' })
+    }
+    // if (!user) {
+    //     return next(new ErrorHandler('Invalid Email or Password', 401));
+    // }
+
+    // Checks if password is correct or not
+    const isPasswordMatched = await user.comparePassword(password);
+
+    // if (!isPasswordMatched) {
+    //     return next(new ErrorHandler('Invalid Email or Password', 401));
+    // }
+    if (!isPasswordMatched) {
+        return res.status(401).json({ message: 'Invalid Email or Password' })
+    }
+    // const token = user.getJwtToken();
+
+    //  res.status(201).json({
+    //  	success:true,
+
+    //  	token
+    //  });
+    sendToken(user, 200, res)
+}
+
+exports.Logout = async (req, res, next) => {
+    res.cookie('token', null, {
+        expires: new Date(Date.now()),
+        httpOnly: true
+    })
+
+    res.status(200).json({
+        success: true,
+        message: 'Logged out'
+    })
+}
+
+exports.ForgotPassword = async (req, res, next) => {
+    const user = await User.findOne({ email: req.body.email });
+    if (!user) {
+        return res.status(404).json({ error: 'User not found with this email' })
+        // return next(new ErrorHandler('User not found with this email', 404));
+    }
+    // Get reset token
+    const resetToken = user.getResetPasswordToken();
+    await user.save({ validateBeforeSave: false });
+    // Create reset password url
+    const resetUrl = `${req.protocol}://localhost:3000/password/reset/${resetToken}`;
+    const message = `Your password reset token is as follow:\n\n${resetUrl}\n\nIf you have not requested this email, then ignore it.`
+    try {
+        await sendEmail({
+            email: user.email,
+            subject: 'Baghun Password Recover',
+            message
+        })
+
+        res.status(200).json({
+            success: true,
+            message: `Email sent to: ${user.email}`
+        })
+
+    } catch (error) {
+        user.resetPasswordToken = undefined;
+        user.resetPasswordExpire = undefined;
+        await user.save({ validateBeforeSave: false });
+        return res.status(500).json({ error: error.message })
+        // return next(new ErrorHandler(error.message, 500))
+    }
+}
+
+exports.ResetPassword = async (req, res, next) => {
+    // Hash URL token
+    const resetPasswordToken = crypto.createHash('sha256').update(req.params.token).digest('hex')
+    const user = await User.findOne({
+        resetPasswordToken,
+        resetPasswordExpire: { $gt: Date.now() }
+    })
+
+    if (!user) {
+        return res.status(400).json({ message: 'Password reset token is invalid or has been expired' })
+        // return next(new ErrorHandler('Password reset token is invalid or has been expired', 400))
+    }
+
+    if (req.body.password !== req.body.confirmPassword) {
+        return res.status(400).json({ message: 'Password does not match' })
+        // return next(new ErrorHandler('Password does not match', 400))
+    }
+
+    // Setup new password
+    user.password = req.body.password;
+    user.resetPasswordToken = undefined;
+    user.resetPasswordExpire = undefined;
+    await user.save();
+    sendToken(user, 200, res);
 }
--- backend/routes/user.js
+++ backend/routes/user.js
@@ -2,9 +2,15 @@ const express = require('express');
 const router = express.Router();
 const upload = require("../utils/multer");
 
-const { registerUser} = require('../controllers/userController');
+const { registerUser, LoginUser, Logout, ForgotPassword, ResetPassword } = require('../controllers/userController');
 const { isAuthenticatedUser, authorizeRoles } = require('../middleware/auth');
 
 router.post('/register', upload.single("avatar"), registerUser);
+router.post('/login', LoginUser);
+router.get('/logout', Logout);
+
+router.post('/forgot', ForgotPassword);
+router.put('/password/reset/:token', ResetPassword);
+
 
 module.exports = router;
