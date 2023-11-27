deee44fd42e028121be801072cc6e7e68ba7cf9c
John Radilh Mancao <117659283+kyutierads@users.noreply.github.com>
2023-11-27 14:54:17 +0800

    cart

--- frontend/src/components/cart/cart.js
+++ frontend/src/components/cart/cart.js
@@ -1,97 +1,118 @@
-import React, { useState, Fragment } from 'react';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState, Fragment } from 'react'
+import { Link } from 'react-router-dom'
+import MetaData from '../layout/MetaData'
+import { useParams, useNavigate } from 'react-router-dom'
 import DeleteIcon from '@mui/icons-material/Delete';
+import LoginModal from '../user/login';
 import { getUser } from '../../utils/helpers';
-import MetaData from '../layout/MetaData';
 
-const Cart = ({ cartItems, removeItemFromCart, setIsLoginOpen, addItemToCart }) => {
-  const navigate = useNavigate();
 
-  const increaseQty = (id, quantity, stock) => {
-    const newQty = quantity + 1;
-    if (newQty > stock) return;
-    addItemToCart(id, newQty);
-  };
+const Cart = ({ addItemToCart, cartItems, removeItemFromCart, setIsLoginOpen, isLoginOpen }) => {
+    const navigate = useNavigate()
 
-  const decreaseQty = (id, quantity) => {
-    const newQty = quantity - 1;
-    if (newQty <= 0) return;
-    addItemToCart(id, newQty);
-  };
-
-  const removeCartItemHandler = (id) => {
-    removeItemFromCart(id);
-  };
-
-  const checkoutHandler = () => {
-    if (getUser()) {
-      navigate('/shipping');
-    } else {
-      setIsLoginOpen(true);
+    const increaseQty = (id, quantity, stock) => {
+        const newQty = quantity + 1;
+        if (newQty > stock) return;
+        addItemToCart(id, newQty);
     }
-  };
 
-  localStorage.setItem('cartItems', JSON.stringify(cartItems));
+    const decreaseQty = (id, quantity) => {
+        const newQty = quantity - 1;
+        if (newQty <= 0) return;
+        addItemToCart(id, newQty);
+    }
 
-  return (
-    <Fragment>
-      <MetaData title={'Your Cart'} />
-      {cartItems.length === 0 ? (
-        <h2 className="mt-5">Your Cart is Empty</h2>
-      ) : (
+    const removeCartItemHandler = (id) => {
+        removeItemFromCart(id)
+    }
+    const [loggedIn, setLoggedIn] = useState(false);
+    const [showLoginModal, setShowLoginModal] = useState(false);
+    const checkoutHandler = () => {
+        if (getUser()) {
+            navigate('/shipping')
+        } else {
+            setIsLoginOpen(true)
+            
+            // alert('wala')
+        }
+    }
+
+    localStorage.setItem('cartItems', JSON.stringify(cartItems))
+
+    return (
         <Fragment>
-          <h2 className="mt-5">Your Cart: <b>{cartItems.length} items</b></h2>
+            <MetaData title={'Your Cart'} />
+            {cartItems.length === 0 ? <h2 className="mt-5">Your Cart is Empty</h2> : (
+                <Fragment>
+                    <h2 className="mt-5">Your Cart: <b>{cartItems.length} items</b></h2>
 
-          <div className="row d-flex justify-content-between">
-            <div className="col-12 col-lg-8">
-              {cartItems.map(item => (
-                <Fragment key={item.product}>
-                  <hr />
-                  <div className="cart-item">
-                    <div className="row">
-                      <div className="col-4 col-lg-3">
-                        <img src={item.image} alt="Product" className="img-fluid" />
-                      </div>
-                      <div className="col-5 col-lg-3">
-                        <Link to={`/products/${item.product}`}>{item.name}</Link>
-                      </div>
-                      <div className="col-4 col-lg-2 mt-4 mt-lg-0">
-                        <p id="card_item_price">${item.price}</p>
-                      </div>
-                      <div className="col-4 col-lg-2 mt-2 mt-lg-0">
-                        <div className="stockCounter d-inline">
-                          <span className="btn btn-danger minus" onClick={() => decreaseQty(item.product, item.quantity)}>-</span>
-                          <input type="number" className="form-control count d-inline" value={item.quantity} readOnly />
-                          <span className="btn btn-primary plus" onClick={() => increaseQty(item.product, item.quantity, item.stock)}>+</span>
+                    <div className="row d-flex justify-content-between">
+                        <div className="col-12 col-lg-8">
+
+                            {cartItems.map(item => (
+                                <Fragment>
+                                    <hr />
+
+                                    <div className="cart-item" key={item.product}>
+                                        <div className="row">
+                                            <div className="col-4 col-lg-3">
+                                                <img src={item.image} alt="Laptop" height="90" width="115" />
+                                            </div>
+
+                                            <div className="col-5 col-lg-3">
+                                                <Link to={`/products/${item.product}`}>{item.name}</Link>
+                                            </div>
+
+
+                                            <div className="col-4 col-lg-2 mt-4 mt-lg-0">
+                                                <p id="card_item_price">${item.price}</p>
+                                            </div>
+
+                                            <div className="col-4 col-lg-2 mt-2 mt-lg-0">
+                                                <div className="stockCounter d-inline">
+                                                    <span className="btn btn-danger minus" onClick={() => decreaseQty(item.product, item.quantity)}>-</span>
+
+                                                    <input type="number" className="form-control count d-inline" value={item.quantity} readOnly />
+
+                                                    <span className="btn btn-primary plus" onClick={() => increaseQty(item.product, item.quantity, item.stock)}>+</span>
+                                                </div>
+                                            </div>
+
+                                            {/* <div className="col-4 col-lg-1 mt-4 mt-lg-0">
+                                                <i id="delete_cart_item" className="fa fa-trash btn btn-danger" onClick={() => removeCartItemHandler(item.product)} ></i>
+                                                <i id="delete_cart_item" className="fa fa-trash btn btn-danger" ></i>
+                                            </div> */}
+                                            <DeleteIcon fontSize='large' className="btn btn-danger py-1 px-2 ml-2 mr-5" onClick={() => removeCartItemHandler(item.product)}>
+                                                <i className="fa fa-trash"></i>
+                                            </DeleteIcon>
+
+                                        </div>
+                                    </div>
+                                    <hr />
+                                </Fragment>
+                            ))}
+
                         </div>
-                      </div>
-                      <div className="col-4 col-lg-1 mt-2 mt-lg-0">
-                        <DeleteIcon fontSize='large' className="btn btn-danger py-1 px-2 ml-2 mr-5" onClick={() => removeCartItemHandler(item.product)}>
-                          <i className="fa fa-trash"></i>
-                        </DeleteIcon>
-                      </div>
+
+                        <div className="col-12 col-lg-3 my-4">
+                            <div id="order_summary">
+                                <h4>Order Summary</h4>
+                                <hr />
+                                <p>Subtotal:  <span className="order-summary-values">{cartItems.reduce((acc, item) => (acc + Number(item.quantity)), 0)} (Units)</span></p>
+                                <p>Est. total: <span className="order-summary-values">${cartItems.reduce((acc, item) => acc + item.quantity * item.price, 0).toFixed(2)}</span></p>
+
+
+                                <hr />
+                                <button id="checkout_btn" className="btn btn-primary btn-block" onClick={checkoutHandler}>Check out</button>
+
+                                {/*<button id="checkout_btn" className="btn btn-primary btn-block" >Check out</button>*/}
+                            </div>
+                        </div>
                     </div>
-                  </div>
-                  <hr />
                 </Fragment>
-              ))}
-            </div>
-
-            <div className="col-12 col-lg-3 my-4">
-              <div id="order_summary">
-                <h4>Order Summary</h4>
-                <hr />
-                <p>Subtotal: <span className="order-summary-values">{cartItems.reduce((acc, item) => (acc + Number(item.quantity)), 0)} (Units)</span></p>
-                <p>Est. total: <span className="order-summary-values">${cartItems.reduce((acc, item) => acc + item.quantity * item.price, 0).toFixed(2)}</span></p>
-                <hr />
-                <button id="checkout_btn" className="btn btn-primary btn-block" onClick={checkoutHandler}>Check out</button>
-              </div>
-            </div>
-          </div>
+            )}
         </Fragment>
-      )}
-    </Fragment>
-  );
-};
+    )
+}
 
-export default Cart;
+export default Cart
