476f22748336eef5a5265ce7b9e921088c83afbc
mdelda <98458999+mdelda@users.noreply.github.com>
2023-11-28 05:32:14 +0800

    Update navigation items and sidebar stylin.

--- frontend/src/components/Admin/Sidebar.js
+++ frontend/src/components/Admin/Sidebar.js
@@ -1,62 +1,94 @@
-
-import React from 'react';
-import { Link } from 'react-router-dom';
-import NavDropdown from 'react-bootstrap/NavDropdown';
-import Card from 'react-bootstrap/Card';
-import { Dropdown } from 'react-bootstrap';
-
+import React from "react";
+import { Link } from "react-router-dom";
+import NavDropdown from "react-bootstrap/NavDropdown";
+import Card from "react-bootstrap/Card";
+import { Dropdown } from "react-bootstrap";
 
 const Sidebar = () => {
-    return (
-        <div className="sidebar" style={{ marginTop: '60px' }}>
-          <nav id="sidebar">
-            <ul className="list-unstyled components">
+  return (
+    <div className="sidebar" style={{ marginTop: "60px" }}>
+      <nav id="sidebar">
+        <ul className="list-unstyled components">
+          <Dropdown as="li">
+            <Dropdown.Toggle
+              as={Link}
+              to="#"
+              className="dropdown-item sidebar-link"
+            >
+              Product
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item
+                as={Link}
+                to="/admin/product/new"
+                className="sidebar-link"
+                style={{ color: "black", transition: "color 0.3s" }}
+                onMouseOver={(e) => (e.target.style.color = "white")}
+                onMouseOut={(e) => (e.target.style.color = "black")}
+              >
+                Create
+              </Dropdown.Item>
 
-    
-              <Dropdown as="li">
-                <Dropdown.Toggle as={Link} to="#" className="dropdown-item sidebar-link">
-                  Product
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item as={Link} to="/admin/product/new" className="sidebar-link">
-                    Create
-                  </Dropdown.Item>
-                  <Dropdown.Item as={Link} to="/admin/product" className="sidebar-link">
-                    Product
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-    
-              <li>
-                <Link to="/admin/orders" className="sidebar-link">
-                  Orders
-                </Link>
-              </li>
-    
-              <li>
-                <Link to="/admin/users" className="sidebar-link">
-                  Users
-                </Link>
-              </li>
-    
-              <Dropdown as="li">
-                <Dropdown.Toggle as={Link} to="#" className="dropdown-item sidebar-link">
-                  Category
-                </Dropdown.Toggle>
-                <Dropdown.Menu>
-                  <Dropdown.Item as={Link} to="/admin/category" className="sidebar-link">
-                    Category
-                  </Dropdown.Item>
-                  <Dropdown.Item as={Link} to="/admin/category/new" className="sidebar-link">
-                    Create
-                  </Dropdown.Item>
-                </Dropdown.Menu>
-              </Dropdown>
-            </ul>
-          </nav>
-        </div>
-      );
-      
-}
+              <Dropdown.Item
+                as={Link}
+                to="/admin/product"
+                className="sidebar-link"
+                style={{ color: "black", transition: "color 0.3s" }}
+                onMouseOver={(e) => (e.target.style.color = "white")}
+                onMouseOut={(e) => (e.target.style.color = "black")}
+              >
+                Product
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+
+          <li>
+            <Link to="/admin/orders" className="sidebar-link">
+              Orders
+            </Link>
+          </li>
+
+          <li>
+            <Link to="/admin/users" className="sidebar-link">
+              Users
+            </Link>
+          </li>
+
+          <Dropdown as="li">
+            <Dropdown.Toggle
+              as={Link}
+              to="#"
+              className="dropdown-item sidebar-link"
+            >
+              Category
+            </Dropdown.Toggle>
+            <Dropdown.Menu>
+              <Dropdown.Item
+                as={Link}
+                to="/admin/category"
+                className="sidebar-link"
+                style={{ color: "black", transition: "color 0.3s" }}
+                onMouseOver={(e) => (e.target.style.color = "white")}
+                onMouseOut={(e) => (e.target.style.color = "black")}
+              >
+                Category
+              </Dropdown.Item>
+              <Dropdown.Item
+                as={Link}
+                to="/admin/category/new"
+                className="sidebar-link"
+                style={{ color: "black", transition: "color 0.3s" }}
+                onMouseOver={(e) => (e.target.style.color = "white")}
+                onMouseOut={(e) => (e.target.style.color = "black")}
+              >
+                Create
+              </Dropdown.Item>
+            </Dropdown.Menu>
+          </Dropdown>
+        </ul>
+      </nav>
+    </div>
+  );
+};
 
 export default Sidebar;
